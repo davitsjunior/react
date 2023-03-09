40e88d6a483e67ad843ac5e450c03f7c1976980b
davijuniorklab <davi.junior@klab.com.br>
2023-03-09 11:51:09 -0300

    Aula 50 completa

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,46 +1,30 @@
-import { useState, useEffect } from 'react';
+import P from 'prop-types';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 
-const eventFn = () => {
-  console.log('JavaScript Puro');
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('Filho redenrizou');
+  return <button onClick={() => incrementButton(100)}>+</button>;
+});
+
+Button.propTypes = {
+  incrementButton: P.func,
 };
 
-//"COMPONENTE STATLENT (SEM ESTADO)"
 function App() {
   const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
 
-  /* //componentDidUpdate - executa toda vez que o component atualiza
-  useEffect(() => {
-    console.log('ComponentDidUpdate');
-  });
-
-  //componentDidMount - executa um vez
-  useEffect(() => {
-    console.log('ComponentDidMount');
-  }, []); */
-
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    //componenteWillUmount - Limpa Listener
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
   }, []);
 
-  //Com dependência - executa toda vez que a dependência mudar
-  useEffect(() => {
-    console.log('Counter1:', counter, 'Counter2:', counter2);
-  }, [counter, counter2]);
+  console.log('Pai redenrizou');
 
   return (
     <div className="App">
-      <h1>
-        C1: {counter} C2: {counter2}
-      </h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter2(counter2 + 1)}>+2</button>
+      <p>Teste 03</p>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }

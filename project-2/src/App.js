418ccb4afa84c26e0ed711353c2747a1bcee364d
davijuniorklab <davi.junior@klab.com.br>
2023-03-10 11:08:58 -0300

    useMemo concluido 4min. aula 51

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,11 +1,11 @@
 import P from 'prop-types';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.css';
 
-const Button = React.memo(function Button({ incrementButton }) {
-  console.log('Filho redenrizou');
+const Button = ({ incrementButton }) => {
+  console.log('Filho renderizou');
   return <button onClick={() => incrementButton(100)}>+</button>;
-});
+};
 
 Button.propTypes = {
   incrementButton: P.func,
@@ -20,11 +20,15 @@ function App() {
 
   console.log('Pai redenrizou');
 
+  const btn = useMemo(() => {
+    return <Button incrementButton={incrementCounter} />;
+  }, [incrementCounter]);
+
   return (
     <div className="App">
       <p>Teste 03</p>
       <h1>C1: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+      {btn}
     </div>
   );
 }

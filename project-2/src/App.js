842a2303840fda3550a836b184a5dbb89f4b0853
davijuniorklab <davi.junior@klab.com.br>
2023-03-17 10:22:24 -0300

    Aula 54 concluída com problema no H1

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,60 +1,12 @@
-//import P from 'prop-types';
-//import { useEffect, useMemo, useState, useRef } from 'react';
-import React, { useContext, useState } from 'react';
 import './App.css';
-// eslint-disable-next-line
-const globalState = {
-  title: 'O título do contexto',
-  body: 'O Body do contexto',
-  counter: 0,
-};
-// eslint-disable-next-line
-const GlobalContext = React.createContext();
-
-// eslint-disable-next-line
-const Div = ({ children }) => {
-  return (
-    <>
-      <H1 />
-      <P />
-    </>
-  );
-};
-
-// eslint-disable-next-line
-const H1 = () => {
-  const theContext = useContext(GlobalContext);
-  const {
-    contextState: { title, counter },
-  } = theContext;
-  return (
-    <h1>
-      {title} {counter}
-    </h1>
-  );
-};
-
-const P = () => {
-  const theContext = useContext(GlobalContext);
-  const {
-    contextState: { body },
-    setContextState,
-  } = theContext;
-  return (
-    <p
-      onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}
-    >
-      {body}
-    </p>
-  );
-};
+import { AppContext } from './context/AppContext';
+import { Div } from './components/Div';
 
 function App() {
-  const [contextState, setContextState] = useState(globalState);
   return (
-    <GlobalContext.Provider value={{ contextState, setContextState }}>
+    <AppContext>
       <Div />
-    </GlobalContext.Provider>
+    </AppContext>
   );
 }
 
